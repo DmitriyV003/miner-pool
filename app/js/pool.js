4b2a473cd3d0d95d48cac82fc37724fe5063c8a9
dmitriy <vdmitriy003@gmail.com>
2022-01-31 00:44:48 +0300

    feat: add pool page + adaptive without page's header

---
+++ app/js/pool.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    // $('.js-pool-header-btns').slick({
+    //     slidesToShow: 6,
+    //     slidesToScroll: 1,
+    //     dots: false,
+    //     arrows: false,
+    //     fade: true,
+    //     infinite: true,
+    //     variableWidth: true
+    // })
+});
---
+++ build/js/pool.js
@@ -0,0 +1,12 @@
+"use strict";
+
+$(document).ready(function () {// $('.js-pool-header-btns').slick({
+  //     slidesToShow: 6,
+  //     slidesToScroll: 1,
+  //     dots: false,
+  //     arrows: false,
+  //     fade: true,
+  //     infinite: true,
+  //     variableWidth: true
+  // })
+});
